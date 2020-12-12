e18ea33d37cbe08a513f46d5193039807c8fa82c
Kieran Sharley <kieransharley@MacBook-Pro.hitronhub.home>
2020-12-12 13:23:20 -0800

    linting

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useApplicationData = function () {
+
+export default function useApplicationData(props) {
+  const setDay = day => setState(prev => ({ ...prev, day }));
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -19,140 +21,156 @@ const useApplicationData = function () {
             id: 1,
             name: "Sylvia Palmer",
             avatar: "https://i.imgur.com/LpaY82x.png",
-          },
-        },
+          }
+        }
       },
       {
         id: 3,
         time: "2pm",
         interview: {
-          student: "Paul Chen",
+          student: "Matthew Salakas",
           interviewer: {
-            id: 2,
-            name: "Tori Malcolm",
-            avatar: "https://i.imgur.com/LpaY82x.png",
-          },
-        },
+            id: 5,
+            name: "Sven Jones",
+            avatar: "https://i.imgur.com/twYrpay.jpg",
+          }
+        }
       },
       {
         id: 4,
         time: "3pm",
         interview: {
-          student: "Scott Morton",
+          student: "Luke Woodnutt",
           interviewer: {
-            id: 3,
-            name: "Mildred Nazir",
-            avatar: "https://i.imgur.com/LpaY82x.png",
-          },
-        },
+            id: 2,
+            name: "Tori Malcolm",
+            avatar: "https://i.imgur.com/Nmx0Qxo.png",
+          }
+        }
       },
-    ],
-    interviewers: [
       {
+        id: 5,
+        time: "4pm",
+        interview: {
+          student: "Courtney Hulbert",
+          interviewer: {
+            id: 1,
+            name: "Sylvia Palmer",
+            avatar: "https://i.imgur.com/LpaY82x.png",
+          }
+        }
+      }
+    ],
+
+    interviewers: [{
+      "1": {
         id: 1,
         name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
+        avatar: "https://i.imgur.com/LpaY82x.png"
       },
-      {
+      "2": {
         id: 2,
         name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
       },
-      {
+      "3": {
         id: 3,
         name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
+        avatar: "https://i.imgur.com/T2WwVfS.png"
       },
-      { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-      { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
-    ],
+      "4": {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg"
+      },
+      "5": {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg"
+      }
+    }
+    ]
+
   });
 
-  const setDay = (day) => setState({ ...state, day });
-
-  const bookInterview = function (id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    const dayID = getDay(state.day);
-
-    const day = {
-      ...state.days[dayID],
-      spots: state.days[dayID].spots,
-    };
-    if (state.appointments[id].interview === null) {
-      day.spots = day.spots - 1;
-    }
-
-    const days = [...state.days];
-    days[dayID] = day;
-
-    return axios.put(`/api/appointments/${id}`, appointment).then((res) => {
-      setState({
-        ...state,
-        appointments,
-        days,
-      });
-    });
-  };
-
-  const cancelInterview = function (id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    const dayID = getDay(state.day);
-
-    const day = {
-      ...state.days[dayID],
-      spots: state.days[dayID].spots,
-    };
-    day.spots = day.spots + 1;
-    let days = [...state.days];
-    days[dayID] = day;
-
-    return axios.delete(`/api/appointments/${id}`).then((res) => {
-      setState({
-        ...state,
-        appointments,
-        days,
-      });
-    });
-  };
-
-  const getDay = function (name) {
-    const match = (element) => element.name === name;
-    return state.days.findIndex(match);
-  };
 
   useEffect(() => {
     Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`),
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
-
-      setState((prev) => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data,
-      }));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 
-  return { state, setDay, bookInterview, cancelInterview };
-};
-export default useApplicationData;
+  const spotsRemaining = (day) => {
+    const match = (element) => element.name === day;
+    return state.days.findIndex(match);
+  };
+
+  function cancelInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    const dayIndex = spotsRemaining(state.day);
+
+    const dayObj = {
+      ...state.days[dayIndex],
+      spots: state.days[dayIndex].spots // + 1
+    };
+    dayObj.spots = dayObj.spots +1;
+    let days = [...state.days];
+
+    days[dayIndex] = dayObj;
+
+    return axios.delete(`/api/appointments/${id}`)
+      .then((response) => {
+        setState({ ...state, appointments, days });
+      });
+  }
+
+ 
+  function bookInterview(id, interview) {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    const dayIndex = spotsRemaining(state.day);
+
+    const dayObj = {
+      ...state.days[dayIndex],
+      spots: state.days[dayIndex].spots
+    };
+    if (state.appointments[id].interview === null) {
+      dayObj.spots = dayObj.spots -1
+    }
+
+    const days = [...state.days]
+    days[dayIndex] = dayObj;
+
+    return axios.put(`/api/appointments/${id}`, appointment)
+      .then((response) => {
+        setState({ ...state, appointments, days });
+      });
+  }
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview,
+  };
+}
